a6dba2d1d319d79269fcd11f8d347c5c8fd03f54
Fikir <fikirsml@gmail.com>
2024-05-29 11:26:15 +0300

    finished the workflow and pricing secion

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Feature from "./components/Feature"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import Pricing from "./components/Pricing"
+import Workflow from "./components/Workflow"
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
     <div className="max-w-7xl mx-auto pt-20 px-6">
     <Hero/>
     <Feature/>
+    <Workflow/>
+    <Pricing/>
     </div>
     </>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,11 +26,11 @@ const Hero = () => {
 
         </div>
         <div className="flex mt-10 justify-center">
-            <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4">
+            <video autoPlay loop muted className="rounded-lg w-1/2  shadow-orange-400 mx-2 my-4">
                 <source src={video1} type="video/mp4"/>
                 your browser doesnt support the video.
             </video>
-            <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4">
+            <video autoPlay loop muted className="rounded-lg w-1/2   shadow-orange-400 mx-2 my-4">
                 <source src={video2} type="video/mp4"/>
                 your browser doesnt support the video.
             </video>
